e48c9ca863d05e824e6ce406b8ea61dece2e2e1f
sVyu <vyuvyuvyu@naver.com>
2025-01-22 16:25:46 +0900

    feat: Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 movement;
+    Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        //Debug.Log(movement);
+    }
+
+    void HandleMovement() {
+        Vector3 currentPosition = rigidBody.position;
+        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y); // vector2 y -> vector3 z
+        Vector3 newPosition = currentPosition + moveDirection * moveSpeed * Time.deltaTime;
+
+        rigidBody.MovePosition(newPosition);
+    }
+}
