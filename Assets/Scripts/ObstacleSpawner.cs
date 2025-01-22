1f021b28cd79b229d30faef1edf785cc0f441735
sVyu <vyuvyuvyu@naver.com>
2025-01-22 18:53:47 +0900

    feat: Obstacle Prefab Variants

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float obstacleSpawnTime = 1f;
-
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f;
 
     private void Start()
     {
@@ -16,9 +17,11 @@ public class ObstacleSpawner : MonoBehaviour
     {
         while(true)
         {
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
             yield return new WaitForSeconds(obstacleSpawnTime);
             //Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
-            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+            Instantiate(obstaclePrefab, transform.position, Random.rotation, obstacleParent);
         }
     }
 }
