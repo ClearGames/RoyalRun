5f4fbdb3cc896490e923b0ff489b6469f92a0c64
sVyu <vyuvyuvyu@naver.com>
2025-01-23 14:27:53 +0900

    feat: Modifying Physics (when collide with fence or pickup apple)

---
+++ Assets/Scripts/Pickups/Apple.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Apple : Pickup
+{
+    [SerializeField] float adjustChangeMoveSpeedAmount = 3f;
+    LevelGenerator levelGenerator;
+
+    private void Start()
+    {
+        levelGenerator = FindFirstObjectByType<LevelGenerator>();
+    }
+
+    protected override void OnPickup()
+    {
+        levelGenerator.ChangeChunkMoveSpeed(adjustChangeMoveSpeedAmount);
+    }
+}
--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -4,9 +4,16 @@ public class PlayerCollisionHandler : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] float collisionCooldown = 1f;
+    [SerializeField] float adjustChangeMoveSpeedAmount = -2f;
 
     const string hitString = "Hit";
     float cooldownTimer = 0f;
+
+    LevelGenerator levelGenerator;
+    private void Start()
+    {
+        levelGenerator = FindFirstObjectByType<LevelGenerator>();
+    }
 
     private void Update()
     {
@@ -16,6 +23,8 @@ public class PlayerCollisionHandler : MonoBehaviour
     void OnCollisionEnter(Collision other)
     {
         if (cooldownTimer < collisionCooldown) return;
+
+        levelGenerator.ChangeChunkMoveSpeed(adjustChangeMoveSpeedAmount);
         animator.SetTrigger(hitString);
         cooldownTimer = 0f;
     }
